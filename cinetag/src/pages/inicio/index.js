7887e544f52dfbb66ba2c389c37d2e69a5ed4431
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-05-21 22:07:58 -0300

    feat: favorite page

--- cinetag/src/pages/inicio/index.js
+++ cinetag/src/pages/inicio/index.js
@@ -1,25 +1,21 @@
 import Banner from "components/Banner";
-import Cabecalho from "components/Cabecalho";
 import Card from "components/Card";
-import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
 import videos from 'json/db.json';
-import styles from ''
+import styles from './Inicio.module.css';
 
 const Inicio = () => {
     return (
         <>
-            <Cabecalho />
             <Banner imagem="home" />
             <Titulo>
                 <h1>Um lugar para guardar seus vídeos e filmes!</h1>
             </Titulo>
             <section className={styles.container}>
                 {videos.map((video) => {
-                    <Card {...video} key={video.id} />
+                    return <Card {...video} key={video.id} />
                 })}
             </section>
-            <Rodape />
         </>
     )
 }

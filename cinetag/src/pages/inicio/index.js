fb9e71c5608193f10d79ceeeb8f57c67ccff11a3
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-05-24 12:44:39 -0300

    consuming api

--- cinetag/src/pages/inicio/index.js
+++ cinetag/src/pages/inicio/index.js
@@ -1,10 +1,19 @@
 import Banner from "components/Banner";
 import Card from "components/Card";
 import Titulo from "components/Titulo";
-import videos from 'json/db.json';
 import styles from './Inicio.module.css';
+import { useEffect, useState } from "react";
 
 const Inicio = () => {
+    const [videos, setVideos] = useState([]);
+    useEffect(() => {
+        fetch("url da api")
+        .then(resposta => resposta.json())
+        .then(dados => {
+            setVideos(dados)
+        })
+    }, [])
+    
     return (
         <>
             <Banner imagem="home" />
